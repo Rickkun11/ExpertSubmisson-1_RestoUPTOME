35e2c5adc5a421adf1bbcd7c16211d2a06fa5d30
rickkun11 <rickkun11@gmail.com>
2022-12-12 01:41:55 +0700

    first commit

---
+++ webpack.dev.js
@@ -0,0 +1,19 @@
+const {merge} = require('webpack-merge');
+const path = require('path');
+const common = require('./webpack.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+
+    compress: true,
+    port: 8080, // default 8080
+  },
+});
